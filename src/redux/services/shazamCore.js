51495004278e9f960e73b3f88be46859c32b7eee
OscarJGarcia <OscarJavier.GarciaGarcia@marcusmillichap.com>
2023-02-06 14:23:21 -0500

    Update: Add some changes

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,19 +1,19 @@
-import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-    export const shazamCoreApi = createApi({
-        reducerPath: 'shazamCoreApi',
-        baseQuery: fetchBaseQuery({
-            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-            prepareHeaders: (headers) => {
-                headers.set('X-RapidAPI-Key','ad7cae7569msh384c77d9efb709bp1bc70ejsnb1e184d89cad');
-                return headers;
-            }
-        }),
-        endpoints: (builder) => ({
-            getTopCharts: builder.query({query: () => '/charts/world'}),
-        }),
-    })
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+})
 
-    export const {
-        useGetTopChartsQuery,
-    }= shazamCoreApi;
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
